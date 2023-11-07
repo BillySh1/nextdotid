86fc36829841dcc4be52b16ea1bc95c122224a38
billy <Billy@mask.io>
2023-11-07 21:26:14 +0800

    chore: mobile init

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,10 +1,28 @@
+"use client";
 import Image from "next/image";
 import Menu from "./Menu";
 import Button from "./Button";
 import Link from "next/link";
+import useMatchBreakpoints from "@/utils/hooks";
+import { useEffect, useRef, useState } from "react";
 
 export default function Navbar() {
-  return (
+  const { isDesktop } = useMatchBreakpoints();
+  const mobileMenu = useRef<any>(null)
+  const [showOverlay, setShowOverlay] = useState(false);
+    useEffect(()=>{
+      document.addEventListener('click',(e)=>{
+          if(showOverlay &&  mobileMenu.current){
+            if(!mobileMenu.current.contains(e.target)){
+              setShowOverlay(false)
+            }
+          }
+      })
+      return document.removeEventListener('click',()=>{
+        setShowOverlay(false)
+      })
+    },[])
+  return isDesktop ? (
     <div className="navbar">
       <Link href={"/"}>
         <Image
@@ -28,6 +46,29 @@ export default function Navbar() {
           <Image width={20} height={20} src="imgs/arrow-right-up.svg" alt="" />
         </Button>
       </Link>
+    </div>
+  ) : (
+    <div className="navbar">
+      <Link href={"/"}>
+        <Image
+          width={60}
+          height={32}
+          className="logo"
+          src="imgs/menu/logo_mobile.svg"
+          alt="next.id logo"
+        />
+      </Link>
+      <Image
+        style={{
+          cursor: "pointer",
+        }}
+        onClick={() => setShowOverlay(true)}
+        src={"imgs/menu/menu_mobile.svg"}
+        width={24}
+        height={24}
+        alt="menu"
+      />
+      {showOverlay && <div ref={mobileMenu} className="overlay">2</div>}
     </div>
   );
 }
