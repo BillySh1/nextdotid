83ffafc65cc522c14c1267b3d0508cf8c3f9f086
billy <Billy@mask.io>
2023-11-08 17:28:45 +0800

    chore: brand line

--- app/brand/page.tsx
+++ app/brand/page.tsx
@@ -1,4 +1,5 @@
 import Button from "@/component/Button";
+import Link from "next/link";
 import SVG from "react-inlinesvg";
 
 export default function BrandPage() {
@@ -51,9 +52,72 @@ export default function BrandPage() {
     { key: "Eerie Black", name: "Eerie Black", hex: "#141414" },
   ];
 
+  const gradients = [
+    {
+      key: "background: linear-gradient(51deg, #EECB50 43.1%, #55B8FF 61.1%, #1A16B8 74.09%)",
+      colors: [
+        {
+          hex: "#EECB50",
+          stop: "43.1",
+        },
+        {
+          hex: "#55B8FF",
+          stop: "61.1",
+        },
+        {
+          hex: "#1A16B8",
+          stop: "74.09",
+        },
+      ],
+      degree: 51,
+    },
+    {
+      key: "background: var(--gradients-gradient-2, linear-gradient(222deg, #EECB50 5.45%, #70D7E6 78.15%)",
+      colors: [
+        {
+          hex: "#EECB50",
+          stop: "5.45",
+        },
+        {
+          hex: "#70D7E6",
+          stop: "78.15",
+        },
+      ],
+      degree: 222,
+    },
+    {
+      key: "background: var(--gradients-gradient-3, linear-gradient(224deg, #1A16B8 29.93%, #55B8FF 75.86%)",
+      colors: [
+        {
+          hex: "#1A16B8",
+          stop: "29.93",
+        },
+        {
+          hex: "#55B8FF",
+          stop: "75.86",
+        },
+      ],
+      degree: 224,
+    },
+    {
+      key: "background: var(--gradients-gradient-4, linear-gradient(226deg, #EECB50 15.01%, #1A16B8 76.16%)",
+      colors: [
+        {
+          hex: "#EECB50",
+          stop: "15.01",
+        },
+        {
+          hex: "#1A16B8",
+          stop: "76.16",
+        },
+      ],
+      degree: 226,
+    },
+  ];
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="section header">Brand Guidelines</div>
+      <div className="section brand-header">Brand Guidelines</div>
       <div className="section">
         <div className="title">Next.ID Brand</div>
         <div className="row">
@@ -82,19 +146,18 @@ export default function BrandPage() {
               {`The clearspace of our logo is a designated area around the logo where no other elements or text should be placed. This clearspace is crucial to maintain the logo's visual integrity, legibility, and impact, ensuring it stands out effectively in various applications.`}
             </div>
           </div>
-          <div className="item"></div>
         </div>
         <div className="row">
-          <div className="item">
+          <div className="item logodonts">
             <div className="item-title">{`Logo Dont's`}</div>
             <div className="item-content">{`Creating an effective and memorable logo is crucial for brand identity and recognition. Here are some key do's and don'ts for logo design:`}</div>
             <div className="flex">
               {dontsMap.map((x) => {
                 return (
-                  <div key={x.key} className="flex-item">
+                  <div key={x.key} className="logo-item">
                     <img src={x.img} alt="" />
                     <div className="item-title">{x.title}</div>
-                    <div className="itme-content">{x.content}</div>
+                    <div className="item-content">{x.content}</div>
                   </div>
                 );
               })}
@@ -128,12 +191,122 @@ export default function BrandPage() {
             );
           })}
         </div>
+        <div className="flex-title">
+          <div className="title">Gradients</div>
+          <div className="line" />
+        </div>
+        <div className="wrapper center">
+          {gradients.map((x, idx) => {
+            return (
+              <div key={x.key} className="card">
+                <img
+                  className="gradient"
+                  src={`imgs/brand/color${idx}.png`}
+                  alt=""
+                />
+                <div className="gradient-content">
+                  <div className="table">
+                    <div className="item-hex">
+                      <div className="item-title">HEX</div>
+                      {x.colors
+                        .map((x) => x.hex)
+                        .map((i) => {
+                          return (
+                            <div key={`item_${i}`} className="flex">
+                              <div
+                                style={{
+                                  width: ".75rem",
+                                  height: ".75rem",
+                                  background: i,
+                                }}
+                              ></div>
+                              {i}
+                            </div>
+                          );
+                        })}
+                      <div
+                        className="flex"
+                        style={{
+                          alignSelf: "flex-start",
+                        }}
+                      >
+                        <SVG
+                          src="imgs/brand/degree.svg"
+                          width={12}
+                          height={12}
+                        />
+                        {x.degree}°
+                      </div>
+                    </div>
+                    <div className="item-hex">
+                      <div className="item-title">STOP</div>
+                      {x.colors
+                        .map((x) => x.stop)
+                        .map((i) => {
+                          return (
+                            <div key={`item_${i}`} className="flex">
+                              {i}
+                            </div>
+                          );
+                        })}
+                    </div>
+                  </div>
+                  <Button variant="secondary">
+                    PNG <SVG src="imgs/brand/download.svg" />
+                  </Button>
+                  <div className="code">
+                    <p>{x.key} </p>
+                    <SVG
+                      className="action"
+                      width={16}
+                      height={16}
+                      src="imgs/brand/copy.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="section">
-        <div className="title">Assets</div>
-        <Button variant="primary">
-          Download Assets <SVG src="imgs/brand/download.svg" />
-        </Button>
+      <div className="section asset">
+        <div className="title">
+          <div>Assets</div>
+          <Button variant="primary">
+            <Link href={""}>Download Assets</Link>{" "}
+            <SVG src="imgs/brand/download.svg" />
+          </Button>
+        </div>
+        <div className="assets-wrapper">
+          <div className="asset-item">
+            <img src="imgs/brand/logomark.svg" alt="" />
+            <div className="ctx">
+              <div className="ctx-title">Logomark</div>
+              <div className="flex">
+                <Button variant="secondary">
+                  PNG <SVG src="imgs/brand/download.svg" />
+                </Button>
+                <Button variant="secondary">
+                  SVG <SVG src="imgs/brand/download.svg" />
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="asset-item">
+            <img src="imgs/brand/logotype.svg" alt="" />
+            <div className="ctx">
+              <div className="ctx-title">Logotype</div>
+              <div className="flex">
+                <Button variant="secondary">
+                  PNG <SVG src="imgs/brand/download.svg" />
+                </Button>
+                <Button variant="secondary">
+                  SVG <SVG src="imgs/brand/download.svg" />
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
